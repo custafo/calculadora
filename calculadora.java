c5e522dad0fff532e1cd0394aa8ffab25477c81a
GUSTAVO PEREIRA OLIVEIRA LIMA <gustavo.00000844518@unicap.br>
2023-02-14 05:38:02 +0000

    changed som stuff

--- calculadora.java
+++ calculadora.java
@@ -3,13 +3,22 @@ public class calculadora {
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
+        char ch;
+        
+        do{
         System.out.println("Which operation would you like to apply?");
         String input = in.nextLine();
-        
+
+            if(!input.equals("sum") && !input.equals("subtraction") && !input.equals("division") && !input.equals("multiply")){
+            System.out.println("Not an operation, please insert one of the following: sum, subtraction, division or multiply");
+            String input1 = in.nextLine();
+            input.equals(input1);
+            } /////clean it up
+            
         System.out.println("Tell me two numbers to be operated");
         double n1 = in.nextDouble();
         double n2 = in.nextDouble();
-    
+
         if(input.equals("sum")) {
             double sum = n1 + n2;
             System.out.println("The result is: " + sum);
@@ -25,6 +34,10 @@ public class calculadora {
         else if(input.equals("multiply")){
             double multiply = n1 * n2;
             System.out.println("The result is: " + multiply);
-        }  
+        }
+            System.out.println("Do you want to continue? y/n");
+            ch = in.next().charAt(0);
+            ch = Character.toLowerCase(ch);
+        }while(ch == 'y');
     }
     }
